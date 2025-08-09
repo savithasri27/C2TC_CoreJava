b8ef592a754172f39a00d534637d9fcf9a43c342
savithasri27 <savithasrivarshini@gmail.com>
2025-08-09 17:27:30 +0530

    Add files via upload

---
+++ C2TC_SAVITHA/src/Day3/Encapsulation.java
@@ -0,0 +1,19 @@
+package Day3;
+
+public class Encapsulation {
+public static void main(String[] args) {
+		
+	 
+	Encapsulation obj = new Encapsulation();
+		obj.setSerialNum(101);
+		((Object) obj).setName("Pooja");
+		obj.setAge(20);
+		System.out.println(obj);
+		
+	}
+
+private void setSerialNum(int i) {
+	// TODO Auto-generated method stub
+	
+}
+}
